bbe59a6ad1dcf7cc5772eaeb1974b99bd1c83691
thoriqmacto <thariq.hadad@gmail.com>
2023-10-02 23:34:09 +0700

    Comment out the class version

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { render } from 'react-dom';
-// import App from "./components/App";
-import App from './components/AppUseClass';
+import App from './components/App';
+// import App from './components/AppUseClass';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 render(
